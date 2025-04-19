899351226d07460df39ca73a07a33198b41dda55
anele <anele@Nontanda.lan>
2025-04-19 20:33:33 +0200

    unit testing for project

---
+++ src/main/java/group_2/prog5121_project/LogIn.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package group_2.prog5121_project;
+
+/**
+ *
+ * @author anele
+ */
+public class LogIn {
+    
+}
